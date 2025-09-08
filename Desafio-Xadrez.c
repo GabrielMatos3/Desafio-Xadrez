944be8e2aac65b798f1b067f74ab8d6dbc27b884
GabrielMatos3 <gabrielsteam166@gmail.com>
2025-09-08 20:05:00 +0000

    att

--- Desafio-Xadrez.c
+++ Desafio-Xadrez.c
@@ -1,37 +1,59 @@
 #include <stdio.h>
 
+void recursivoTorre(int casas){
+    if(casas > 0){
+        recursivoTorre(casas - 1);
+        printf("se moveu 1 casa para direita\n");
+    }
+}
+
+void recursivoBispo(int casas){
+    if(casas > 0){
+        recursivoBispo(casas - 1);
+        printf("se moveu 1 casa para cima e para direita\n");
+    }
+}
+
+void recursivoRainha(int casas){
+    if(casas > 0){
+        recursivoRainha(casas - 1);
+        printf("se moveu 1 casa para esquerda\n");
+    }
+}
+
 int main(){
+    //declaração de variaveis
+    int cavalo = 0, movimento = 0;
 
-    int torre = 0, bispo = 0, rainha = 0, cavalo = 0;
-    int movimento = 0;
-
-    for(torre; torre <= 5; torre++){
-        printf("A Torre se moveu 1 casa para direita\n");
-    }
-
-    printf("\n");
-
-    while(bispo <= 5){
-        printf("O Bispo se moveu 1 casa para cima e para direita\n");
-        bispo++;
-    }
+    int torre = 5;
+    printf("Torre \n");
+    recursivoTorre(torre);
 
     printf("\n");
 
-    do{
-        printf("A Rainha se moveu 1 casa para esquerda\n");
-        rainha++;
-    }while(rainha <= 5);
+    int bispo = 5;
+    printf("Bispo \n");
+    recursivoBispo(bispo);
+      
 
     printf("\n");
 
+    int rainha = 5;
+    printf("Rainha \n");
+    recursivoRainha(rainha);
+    
+
+    printf("\n");
+    //movimentação do cavalo
+    printf("Cavalo\n");
     while(movimento == 0){
         for (cavalo; cavalo <=1 ; cavalo++)
         {
-            printf("cavalo se moveu 1 casa para cima\n");
+            printf("se moveu 1 casa para cima\n");
+            
         }
 
-        printf("cavalo se moveu 1 casa para direita");
+        printf("se moveu 1 casa para direita");
         movimento++;
         printf("\n");
         
